4d4f3a6dcc9cfcb75d775338891c3a50e00fc3b2
Gordon <gordonlarrigan@gmail.com>
2021-12-07 15:33:42 +0000

    Store secrets in env file

--- aoc.sh
+++ aoc.sh
@@ -3,7 +3,7 @@
 year=2021;
 
 # Get this from 'session' cookie in Chrome devtools
-cookie=53616c7465645f5f6e1a467b7b454a846908f1f3ef3e34f5b6190cb06b2e6a04438adfe99dea7ada7fe3d84fdbd29c8c;
+export $(egrep -v '^#' .env | xargs)
 
 directory=$(printf "%02d" $1)
 
@@ -14,4 +14,4 @@ touch ./$directory/part2.js
 touch ./$directory/instructions.txt
 
 # Get input
-curl "https://adventofcode.com/$year/day/$1/input" -H "cookie: session=$cookie" -o "./$directory/input.txt" 2>/dev/null
+curl "https://adventofcode.com/$year/day/$1/input" -H "cookie: session=$COOKIE" -o "./$directory/input.txt" 2>/dev/null
